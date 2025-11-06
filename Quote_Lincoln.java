9bf204852fcf9a84d70f0437b667bf84819ec37d
Van Nguyen _ Bachelor of Computer Science <152344203+Minhvan1705@users.noreply.github.com>
2025-11-06 17:41:12 -0500

    Add files via upload

---
+++ Quote_Lincoln.java
@@ -0,0 +1,18 @@
+
+/**
+ * Create a BlueJ Project, Print two sentences to the screen 
+ * is a Quote by Abraham Lincoln
+ * @author (Van Nguyen)
+ * @version (October 25,2025)
+ */
+public class Quote_Lincoln
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args)
+    {
+        System.out.println("A quote by Abraham Lincoln: ");  /* output to screen   
+                                                               two string */  
+        System.out.println("What ever you are, be a good one."); 
+    
+    }
+}
